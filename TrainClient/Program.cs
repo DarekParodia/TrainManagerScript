69252cf0aa75d4c667266f4567425fed9a9496d8
DarekParodia <darekparodia@gmail.com>
2024-08-08 22:39:00 +0200

    initial manual movement working

---
+++ TrainClient/Program.cs
@@ -0,0 +1,167 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private string trainNameMOSI = "train_1_mosi"; // ID used to transmit IGC messages from host to client
+        private string trainNameMISO = "train_1_miso"; // ID used to receive IGC messages from client to host
+        private string thrusterForwardGroup = "CargoOne.ThrusterForward"; // Group containing thrusters that will be used for going forward
+        private string thrusterBackwardGroup = "CargoOne.ThrusterBackward"; // Group containing thrusters that will be used for going backward
+        
+        IMyBroadcastListener _broadcastReceiver;
+        
+        List<IMyThrust> thrusterForward = new List<IMyThrust>();
+        List<IMyThrust> thrusterBackward = new List<IMyThrust>();
+        private enum dataType : byte
+        {
+            PING = 0,
+            SPEED = 1
+        }
+        private struct message
+        {
+            public string cargoID;
+            public dataType type;
+            public string data;
+        }
+
+        public Program()
+        {
+            Echo("Starting Train Client...");
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            _broadcastReceiver = IGC.RegisterBroadcastListener(trainNameMOSI);
+            _broadcastReceiver.SetMessageCallback(trainNameMOSI);
+            
+            // set thrusters
+            List<IMyBlockGroup> groups = new List<IMyBlockGroup>();
+            GridTerminalSystem.GetBlockGroups(groups);
+            foreach (IMyBlockGroup group in groups)
+            {
+                if (group.Name == thrusterForwardGroup)
+                {
+                    group.GetBlocksOfType(thrusterForward);
+                }
+                else if (group.Name == thrusterBackwardGroup)
+                {
+                    group.GetBlocksOfType(thrusterBackward);
+                }
+            }
+            Echo("Train Client");
+        }
+        
+        private void sendToMaster(dataType type, string data)
+        {
+            Echo("Sending to master: " + (byte)type + " " + data);
+            IGC.SendBroadcastMessage(trainNameMISO, Me.CubeGrid.CustomName + ";" + (byte)type + ";" + data);
+        }
+        private message parseIGCMessage(MyIGCMessage myIGCMessage)
+        {
+            message msg = new message();
+            string[] data = myIGCMessage.Data.ToString().Split(';');
+            msg.cargoID = data[0];
+            msg.type = (dataType)Enum.Parse(typeof(dataType), data[1]);
+            // rest goes to data even if there are more than 3 elements
+            for (int i = 2; i < data.Length; i++)
+            {
+                msg.data += data[i];
+                if (i < data.Length - 1)
+                    msg.data += ";";
+            }
+            return msg;
+        }
+        private void parsePingMessage(message msg)
+        {
+            Vector3 position = Me.GetPosition();
+            sendToMaster(dataType.PING, "pong");
+        }
+        private void parseSpeedMessage(message msg)
+        {
+            Echo("data: " + msg.data);
+            
+            string[] data = msg.data.Split(';');
+            float x = float.Parse(data[0]);
+            float y = float.Parse(data[1]);
+            float z = float.Parse(data[2]);
+            // Vector3 speed = new Vector3(x, y, z);
+            // Echo("Speed: " + speed.ToString());
+            
+            // set thrusters
+
+            if (z == 0)
+            {
+                foreach (IMyThrust thruster in thrusterForward)
+                {
+                    thruster.ThrustOverridePercentage = 0;
+                }
+
+                foreach (IMyThrust thruster in thrusterBackward)
+                {
+                    thruster.ThrustOverridePercentage = 0;
+                }
+            }
+            else if (z > 0)
+            {
+                foreach (IMyThrust thruster in thrusterForward)
+                {
+                    thruster.ThrustOverridePercentage = 0;
+                }
+                foreach (IMyThrust thruster in thrusterBackward)
+                {
+                    thruster.ThrustOverridePercentage = z;
+                }
+            }
+            else
+            {
+                foreach (IMyThrust thruster in thrusterForward)
+                {
+                    thruster.ThrustOverridePercentage = -z;
+                }
+                foreach (IMyThrust thruster in thrusterBackward)
+                {
+                    thruster.ThrustOverridePercentage = 0;
+                }
+            }
+            
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            while (_broadcastReceiver.HasPendingMessage)
+            {
+                MyIGCMessage myIGCMessage = _broadcastReceiver.AcceptMessage();
+                message msg = this.parseIGCMessage(myIGCMessage);
+                
+                if (msg.cargoID != Me.CubeGrid.CustomName && msg.cargoID != "all") // skip if message is not for this cargo
+                    continue;
+                
+                switch (msg.type)
+                {
+                    case dataType.PING:
+                        parsePingMessage(msg);
+                        break;
+                    case dataType.SPEED:
+                        parseSpeedMessage(msg);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ TrainHost/Program.cs
@@ -0,0 +1,185 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private string trainNameMOSI = "train_1_mosi"; // ID used to transmit IGC messages from host to client
+        private string trainNameMISO = "train_1_miso"; // ID used to receive IGC messages from client to host
+        private string cockpitStringTag = "[TrainHost]"; // Tag used to identify the cockpit
+        
+        IMyShipController cockpit;
+        IMyBroadcastListener _broadcastReceiver;
+        
+        private mode currentMode = mode.MANUAL;
+        
+        private struct message
+        {
+            public string cargoID;
+            public dataType type;
+            public string data;
+        }
+
+        private struct cargo
+        {
+            public string cargoID;
+            public Vector3 position;
+        }
+        private enum mode
+        {
+            AUTO,
+            MANUAL
+        }
+
+        private enum dataType : byte
+        {
+            PING = 0,
+            SPEED = 1
+        }
+
+        private List<cargo> cargo_list = new List<cargo>();
+        int counter = 0;
+        
+        public Program()
+        {
+            Echo("Starting Train Host...");
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            
+            _broadcastReceiver = IGC.RegisterBroadcastListener(trainNameMISO);
+            _broadcastReceiver.SetMessageCallback(trainNameMISO);
+            
+            // search for cockpit
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+            foreach (IMyTerminalBlock block in blocks)
+            {
+                if (block.CustomName.Contains(cockpitStringTag))
+                {
+                    cockpit = block as IMyShipController;
+                    break;
+                }
+            }
+
+            if (cockpit == null)
+            {
+                throw new Exception("No cockpit found with tag: " + cockpitStringTag);
+            }
+            
+            Echo("Train Host Active");
+            this.ping();
+        }
+        private message parseIGCMessage(MyIGCMessage myIGCMessage)
+        {
+            message msg = new message();
+            string[] data = myIGCMessage.Data.ToString().Split(';');
+            msg.cargoID = data[0];
+            msg.type = (dataType)Enum.Parse(typeof(dataType), data[1]);
+            // rest goes to data even if there are more than 3 elements
+            for (int i = 2; i < data.Length; i++)
+            {
+                msg.data += data[i];
+            }
+            return msg;
+        }
+        
+        private void TransmitData(string cargoID, dataType type, string data)
+        {
+            IGC.SendBroadcastMessage(trainNameMOSI
+                , cargoID + ";" + (byte)type + ";" + data);
+        }
+
+        private void ping()
+        {
+            this.TransmitData("all", dataType.PING, "ping");
+            
+            // receive ping
+            this.cargo_list.Clear();
+
+            while (_broadcastReceiver.HasPendingMessage)
+            {
+                MyIGCMessage myIGCMessage = _broadcastReceiver.AcceptMessage();
+                message msg = parseIGCMessage(myIGCMessage);
+
+                if (msg.type == dataType.PING)
+                {
+                    Echo("Ping received");
+                    cargo newCargo = new cargo();
+                    newCargo.cargoID = msg.cargoID;
+                    this.cargo_list.Add(newCargo);
+                }
+                
+            }
+            
+            Echo("Cargo list: ");
+            foreach (cargo c in this.cargo_list)
+            {
+                Echo(c.cargoID);
+            }
+        }
+
+        private void sendSpeed(string cargoID, Vector3 speed)
+        {
+            string speedString = speed.X + ";" + speed.Y + ";" + speed.Z;
+            this.TransmitData(cargoID, dataType.SPEED, speedString);
+        }
+
+        public void AutoMode(string argument)
+        {
+
+        }
+        
+        public void ManualMode(string argument)
+        {
+            // check if player is trying to move the train
+            Vector3 MoveIndicator = cockpit.MoveIndicator;
+            this.sendSpeed("all", MoveIndicator);
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            switch (argument) // so that driver can switch between modes if needed
+            {
+                case "auto":
+                    this.currentMode = mode.AUTO;
+                    break;
+                case "manual":
+                    this.currentMode = mode.MANUAL;
+                    break;
+            }
+            
+            if (this.cargo_list.Count == 0)
+            {
+                this.ping();
+                return;
+            }
+            
+            switch (this.currentMode)
+            {
+                case mode.AUTO:
+                    this.AutoMode(argument);
+                    break;
+                case mode.MANUAL:
+                    this.ManualMode(argument);
+                    break;
+            }
+        }
+    }
+}
